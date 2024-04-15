682b08e77dbecccf9ffbe693f3040a5d63faf49e
Tyler Howell <tyler.howell1202@gmail.com>
2024-04-14 19:57:03 -0500

    added all pages and html content

--- devportfolio/urls.py
+++ devportfolio/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import RedirectView
-from devportfolio import views
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
